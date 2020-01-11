4c6bc8034d664e7d5509d71ff126b19226278590
StasAlex <stas.alex28@gmail.com>
2020-01-11 20:11:24 +0200

    user template add to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material';
+import { MatCardModule } from '@angular/material/card';
 
 
 
@@ -44,7 +45,8 @@ import { MatInputModule } from '@angular/material';
     MatFormFieldModule,
     ReactiveFormsModule,
     FormsModule,
-    MatInputModule
+    MatInputModule,
+    MatCardModule
   ],
   providers: [LoginService],
   bootstrap: [AppComponent]
